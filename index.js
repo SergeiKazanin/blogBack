212ba5a3c02a311f87645d7ec35854bbaf18f7f3
SergeiKazanin <serega7667@gmail.com>
2023-07-24 09:15:28 +0700

    added cors

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ const app = express();
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use(cookieParser());
 app.use("/", router);
 app.use(errorMiddleware);
