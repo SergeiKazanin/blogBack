00f2d40de370cfb941295f4ef4e56d7e24110ef7
SergeiKazanin <serega7667@gmail.com>
2023-07-24 11:28:48 +0700

    edit cookie set

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -61,6 +61,8 @@ class UserController {
       res.cookie("refreshToken", userData?.refreshToken, {
         maxAge: 10 * 24 * 60 * 60 * 1000,
         httpOnly: true,
+        sameSite: "none",
+        secure: true,
       });
       return res.json(userData);
     } catch (error) {
