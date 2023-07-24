e98e626fb4d7336e2de03f5f4c9fa946c52b58df
SergeiKazanin <serega7667@gmail.com>
2023-07-24 12:01:25 +0700

    edit user-dto

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -3,10 +3,14 @@ module.exports = class UserDto {
   id;
   isActivated;
   fullName;
+  createdAt;
+  updatedAt;
   constructor(model) {
     this.email = model.email;
     this.id = model._id;
     this.isActivated = model.isActivated;
     this.fullName = model.fullName;
+    this.createdAt = model.createdAt;
+    this.updatedAt = model.updatedAt;
   }
 };
